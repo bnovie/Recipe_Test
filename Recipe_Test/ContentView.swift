b684399c73c027ede2c99947c12d9343a0a0514d
bnovie <brian.novie@gmail.com>
2024-09-11 10:12:43 -0400

    Initial Commit

---
+++ Recipe_Test/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Recipe_Test
+//
+//  Created by Brian Novie on 9/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Recipe_Test/Recipe_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe_TestApp.swift
+//  Recipe_Test
+//
+//  Created by Brian Novie on 9/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Recipe_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
